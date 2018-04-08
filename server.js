c8c17ac184a516b95b0c2ed55fa9b20627ac54ba
Shippey <BENJAMIN_W_SHIPPEY@homedepot.com>
2018-04-08 12:57:35 -0400

    Initial push

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const mongo = require('mongodb').MongoClient;
+const pug = require('pug');
+const format_number = require('number-formatter');
+
+//Connection URL
+const url = 'mongodb://localhost:27017';
+const dbName = 'countyrank';
+
+//set views
+app.set('views','views');
+app.set('view engine','pug');
+
+app.use(express.static('.'));
+
+app.get('/index', function(req,res) {
+  res.render('index');
+})
+
+app.get('/results', function(req,res) {
+  var state = req.query.state
+  var rankby = req.query.rankby
+  var col;
+
+  //pass parameters to MongoDB database query
+  mongo.connect(url, function(err,client) {
+    db = client.db('countyrank');
+    col = db.collection('rankings');
+    if(rankby==='Largest') {
+      col.find({"State": state,"_rank_top":{$lte:10}},{_id:0}).sort({"_rank_top":1}).map(function(doc) {return {"rank":doc._rank_top,"rank_overall":doc.rank_overall_top, "CountyName":doc.CountyName,"LandArea":format_number( "#,##0.##",doc.CountyLandAreaMiles)};})
+        .toArray(function(err,doc) {
+          res.render('results',{array:doc,'state':state, 'header':'Largest Counties'});
+        });
+    } else if(rankby==='Smallest') {
+      col.find({"State":state,"_rank_bott":{$lte:10}},{_id:0}).sort({"_rank_bott":1}).map(function(doc) {return {"rank":doc._rank_bott,"rank_overall":doc.rank_overall_bott, "CountyName":doc.CountyName,"LandArea":format_number( "#,##0.##",doc.CountyLandAreaMiles)};})
+        .toArray(function(err,doc) {
+          res.render('results',{array:doc,'state':state, 'header':'Smallest Counties'});
+        });
+    } else {
+      res.render('index');
+    }
+  });
+})
+
+app.get('*', function(req,res) {
+  res.render('index');
+});
+
+app.listen(process.env.PORT);
