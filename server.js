102d8883f1e3d33ea11ca1070e81edf3e1a88a43
Shippey <BENJAMIN_W_SHIPPEY@homedepot.com>
2018-04-22 14:04:57 -0400

    missing var in variable declaration

--- server.js
+++ server.js
@@ -21,12 +21,10 @@ app.get('/index', function(req,res) {
 app.get('/results', function(req,res) {
   var state = req.query.state
   var rankby = req.query.rankby
-  var col;
 
   //pass parameters to MongoDB database query
-  mongo.connect(process.env.MONGODB_URI, function(err,client) {
-    console.log(client);
-    col = client.collection('rankings');
+  mongo.connect(process.env.MONGODB_URI, function(err,db) {
+    var col = db.collection('rankings');
     if(rankby==='Largest') {
       col.find({"State": state,"_rank_top":{$lte:10}},{_id:0}).sort({"_rank_top":1}).map(function(doc) {return {"rank":doc._rank_top,"rank_overall":doc.rank_overall_top, "CountyName":doc.CountyName,"LandArea":format_number( "#,##0.##",doc.CountyLandAreaMiles)};})
         .toArray(function(err,doc) {
